65123bc6a2817c2f5b5345de671d912e2d7668be
Olivier619 <132900963+Olivier619@users.noreply.github.com>
2026-02-04 13:45:28 +0100

    fix: correct UserContext import path in ImageUploadController

--- src/components/custom/ImageUploadController.jsx
+++ src/components/custom/ImageUploadController.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { UserContext } from "../../../context/UserContext";
+import { UserContext } from "../../context/UserContext";
 import { Button } from "../../ui/button";
 
 export default function ImageUploadController() {
